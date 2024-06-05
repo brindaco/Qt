9304cb37f37949029cb9c5de1145662b77fe3f22
Рычагов Егор Денисович <erychagov@npomis.ru>
2024-06-05 13:17:23 +0300

    chapter12

---
+++ chapter12/WidgetAndView/main.cpp
@@ -0,0 +1,26 @@
+#include <QtWidgets>
+
+int main(int argc, char** argv)
+{
+    QApplication     app(argc, argv);
+    QStringList      lst;
+    QListWidget      lwg;
+    QListWidgetItem* pitem = 0;
+
+    lwg.setIconSize(QSize(48, 48));
+    lst << "Linux" << "Windows" << "MacOSX" << "Android";
+    foreach(QString str, lst) {
+        pitem = new QListWidgetItem(str, &lwg);    
+        pitem->setIcon(QPixmap(":/" + str + ".jpg"));
+    }
+    lwg.setWindowTitle("ListWidget");
+    lwg.show();
+
+    QListView listView;
+    listView.setModel(lwg.model());
+    listView.setSelectionModel(lwg.selectionModel());
+    listView.setWindowTitle("ListView");
+    listView.show();
+
+    return app.exec();
+}
