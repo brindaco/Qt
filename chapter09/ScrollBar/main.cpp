dd85592fc74835499093ca2c985ab59a714111ea
Рычагов Егор Денисович <erychagov@npomis.ru>
2024-06-05 12:23:23 +0300

    chapter09

---
+++ chapter09/ScrollBar/main.cpp
@@ -0,0 +1,24 @@
+#include <QtWidgets>
+
+int main (int argc, char** argv)
+{
+    QApplication app(argc, argv);
+
+    QWidget     wgt;
+    QLCDNumber* plcd = new QLCDNumber(4);
+    QScrollBar* phsb = new QScrollBar(Qt::Horizontal);
+
+    QObject::connect(phsb, SIGNAL(valueChanged(int)), 
+                     plcd, SLOT(display(int))
+                    );
+
+    QVBoxLayout* pvbxLayout = new QVBoxLayout;
+    pvbxLayout->addWidget(plcd);    
+    pvbxLayout->addWidget(phsb); 
+    wgt.setLayout(pvbxLayout);
+
+    wgt.resize(250, 150);
+    wgt.show();
+
+    return app.exec();
+}
