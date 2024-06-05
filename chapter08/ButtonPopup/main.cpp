5def63635f5759e4204c3d115e7fbe6a0c2e3375
Рычагов Егор Денисович <erychagov@npomis.ru>
2024-06-05 12:19:54 +0300

    chapter08

---
+++ chapter08/ButtonPopup/main.cpp
@@ -0,0 +1,17 @@
+#include <QtWidgets>
+
+int main (int argc, char** argv)
+{
+    QApplication app(argc, argv);
+
+    QPushButton  cmd("Menu");
+    QMenu*       pmnu = new QMenu(&cmd);
+    pmnu->addAction("Item1");
+    pmnu->addAction("Item2");
+    pmnu->addAction("&Quit", &app, SLOT(quit()));
+
+    cmd.setMenu(pmnu);
+    cmd.show();
+
+    return app.exec();
+}
