625f79b50b18e8cc03472c538a8a718f61b67aff
Рычагов Егор Денисович <erychagov@npomis.ru>
2024-06-06 14:25:39 +0300

    chapter14

---
+++ chapter14/MouseEvent/main.cpp
@@ -0,0 +1,14 @@
+#include <QtGui>
+#include "MouseObserver.h"
+
+int main(int argc, char** argv)
+{
+    QApplication  app(argc, argv);
+    MouseObserver wgt;
+
+    wgt.resize(250, 130);
+    wgt.show();
+
+    return app.exec();
+}
+
