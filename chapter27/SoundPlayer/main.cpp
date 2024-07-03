ac8026dfaccc997e82c547d28110cc534ff6e873
Рычагов Егор Денисович <erychagov@npomis.ru>
2024-07-03 14:57:45 +0300

    chapter27

---
+++ chapter27/SoundPlayer/main.cpp
@@ -0,0 +1,14 @@
+#include <QApplication>
+#include "SoundPlayer.h"
+
+int main(int argc, char** argv)
+{
+    QApplication app(argc, argv);
+    SoundPlayer  soundPlayer;
+
+    soundPlayer.resize(320, 75);
+    soundPlayer.show(); 
+
+    return app.exec();
+}
+
